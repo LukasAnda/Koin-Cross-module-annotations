7d8bc904c15add4948ca0ba4a3cfc1a713d5d972
Lukáš Anda <lukas.anda@gmail.com>
2025-08-31 13:52:44 +0200

    Update to kotlin 2.2.20-RC

--- shared/di/src/commonMain/kotlin/org/example/app/shared/di/AppModule.kt
+++ shared/di/src/commonMain/kotlin/org/example/app/shared/di/AppModule.kt
@@ -1,8 +1,10 @@
 package org.example.app.shared.di
 
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
 import org.koin.core.annotation.Module
 
 @ComponentScan("org.example**")
+@Configuration
 @Module
 class AppModule
--- shared/feature/sample/src/commonMain/kotlin/org/example/app/shared/feature/sample/SampleClass.kt
+++ shared/feature/sample/src/commonMain/kotlin/org/example/app/shared/feature/sample/SampleClass.kt
@@ -1,6 +1,11 @@
 package org.example.app.shared.feature.sample
 
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
 @Single
 class SampleClass
+
+@Module
+class SampleModule
